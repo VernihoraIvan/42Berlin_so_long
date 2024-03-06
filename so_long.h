a2529d68df84562361d942399c548f970d0ca101
VernihoraIvan <vernihorai@gmail.com>
2024-03-06 18:02:29 +0100

    added initial images for render

--- so_long.h
+++ so_long.h
@@ -6,14 +6,17 @@
 /*   By: iverniho <iverniho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 16:25:23 by iverniho          #+#    #+#             */
-/*   Updated: 2024/03/06 16:21:21 by iverniho         ###   ########.fr       */
+/*   Updated: 2024/03/06 18:01:44 by iverniho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include <mlx.h>
+# define WIDTH 32
+# define HEIGHT 32
+
+// # include <mlx.h>
 # include <stdio.h>
 #include <stdlib.h>
 # include "libft/libft.h"
